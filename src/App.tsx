7b04aaff2b29dea41a9d86941054ea4a23b13047
zhaoteng <1813831021@qq.com>
2025-07-18 17:28:28 +0800

    优化6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { Layout, Menu, theme, Button, Avatar, Dropdown, Space, Typography } from 'antd';
 import {
   DashboardOutlined,
